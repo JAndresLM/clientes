9496c4e043a5802dfbc3736be5247e7258869e19
Lesmed Gutiérrez <lesmedgutierrez@outlook.com>
2016-03-30 21:55:58 -0600

    [FIX]: Odoo standards in files, pep8, readme file

---
+++ README.rst
@@ -0,0 +1,52 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==============
+Clientes
+==============
+
+Este módulo le permite administrar:
+    --Información relacionada con clientes
+
+Installation
+============
+
+To install this module, you need to:
+
+* do this ...
+
+Configuration
+=============
+
+To configure this module, you need to:
+
+* go to ...
+
+Usage
+=====
+
+To use this module, you need to:
+
+* go to ...
+
+Known issues / Roadmap
+======================
+
+* ...
+
+Bug Tracker
+===========
+
+Bugs are tracked on ...
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Firstname Lastname <email.address@example.org>
+* Lesmed Gutiérrez <lesmedgutierrez@outlook.com>
+
--- models/cliente.py
+++ models/cliente.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from openerp import models, fields
+
 
 class Cliente(models.Model):
-	_name = 'clientes.cliente'
-	name = fields.Char(string="Nombre Completo", required=True)
-	empresa = fields.Char(string="Empresa")
-	telefono = fields.Char(string="Teléfono")
-	correo = fields.Char(string="Correo Electrónico")
-	active = fields.Boolean(default=True, string="Activo")
-	provincia=fields.Many2one('clientes.provincia',ondelete='cascade', string="Provincia")
-	canton=fields.Many2one('clientes.canton',ondelete='cascade', string="Cantón")
-	distrito=fields.Many2one('clientes.distrito',ondelete='cascade', string="Distrito")
-	otrassenas = fields.Text(string="Otras Señas")
 
+    _name = 'clientes.cliente'
+    name = fields.Char(string="Nombre Completo", required=True)
+    empresa = fields.Char(string="Empresa")
+    telefono = fields.Char(string="Teléfono")
+    correo = fields.Char(string="Correo Electrónico")
+    active = fields.Boolean(default=True, string="Activo")
+    provincia = fields.Many2one('clientes.provincia', ondelete='cascade',
+                                string="Provincia")
+    canton = fields.Many2one('clientes.canton', ondelete='cascade',
+                             string="Cantón")
+    distrito = fields.Many2one('clientes.distrito', ondelete='cascade',
+                               string="Distrito")
+    otrassenas = fields.Text(string="Otras Señas")
